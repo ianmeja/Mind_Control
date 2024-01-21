818f81e16653d266bb805012f7377dd5f9f73cf8
Justina Vacas Castro <juvacas@itba.edu.ar>
2024-01-21 20:41:44 -0300

    brainflow to spectogram

--- spectogram/brainflow.py
+++ spectogram/brainflow.py
@@ -1,23 +1,41 @@
 import pandas as pd
-
-def process_csv(input_file, output_file, header, delimiter='\t', skip_rows=14):
-    # Read the CSV file into a DataFrame, skipping the first 14 rows
-    df = pd.read_csv(input_file, delimiter=delimiter, skiprows=skip_rows)
-
-    # Add the specified header
-    df.columns = header
-
-    # Write the DataFrame to a new CSV file
-    df.to_csv(output_file, index=False, sep=delimiter)
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
 
 # Specify your input and output file paths
-input_file_path = './files/brainflow.csv'
+input_file_path = './files/brainflow5.csv'
 output_file_path = './files/output5.csv'
 
 # Specify the header for the columns
-column_header = ['Sample Index', 'EXG Channel 0', 'EXG Channel 1', 'EXG Channel 2', 'EXG Channel 3', 'Accel Channel 0', 'Accel Channel 1', 'Accel Channel 2', 'Other', 'Other', 'Other', 'Other', 'Other', 'Timestamp', 'Other', 'Timestamp (Formatted)']
+column_header = ['Sample Index', 'EXG Channel 0', 'EXG Channel 1', 'EXG Channel 2', 'EXG Channel 3', 'Accel Channel 0', 'Accel Channel 1', 'Accel Channel 2', 'Other', 'Other', 'Other', 'Other', 'Other', 'Timestamp', 'Other']
+# Read the CSV file into a DataFrame, skipping the first 14 rows
+df = pd.read_csv(input_file_path, delimiter='\t', skiprows=13)
 
-# Process the CSV file with tab as the delimiter using pandas
-process_csv(input_file_path, output_file_path, column_header)
+# Add the specified header
+df.columns = column_header
 
-print(f"Processing completed. Output saved to {output_file_path}")
+# Write the DataFrame to a new CSV file
+df.to_csv(output_file_path, index=False, sep='\t')
+
+df.to_csv(output_file_path, index=False)
+
+# Convert columns to numeric (if needed)
+channels = ['EXG Channel 0', 'EXG Channel 1', 'EXG Channel 2', 'EXG Channel 3']
+
+# Configuration parameters
+fs = 200  # Sampling rate in Hz
+nperseg = 256  # Window size
+noverlap = nperseg // 2  # Overlap between windows
+
+# Calculate and show the spectrogram for each channel
+for channel in channels:
+    f, t, Sxx = signal.spectrogram(df[channel], fs, nperseg=nperseg, noverlap=noverlap)
+    plt.figure(figsize=(10, 6))
+    plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='auto')
+    plt.colorbar(label='Power (dB)')
+    plt.title(f'Spectrogram - {channel}')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.tight_layout()
+    plt.show()
